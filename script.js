02b834ed9d7388d3fdae3e95db5c64e8e09ddb40
Kabinara <140863361+Kabinara@users.noreply.github.com>
2023-08-25 21:15:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputPassword = document.getElementById("input-password");
+const  = document.getElementById("input-password");
+const inputPassword = document.getElementById("input-password");
+
